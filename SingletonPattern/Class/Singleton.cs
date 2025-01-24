d4c6ea67bc235a76f8000621046215906ba2d313
Satit Porntepanon <st_satitza@hotmail.com>
2025-01-24 14:04:54 +0700

    edit output text

---
+++ SingletonPattern/Class/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace SingletonPattern.Class
+{
+    public sealed class Singleton
+    {
+        private static Singleton _singleton;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new Singleton();
+            }
+
+            return _singleton;
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using SingletonPattern.Class;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Design pattern(singleton pattern)");
+
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            if (singleton1.GetHashCode() == singleton2.GetHashCode())
+            {
+                Console.WriteLine(
+                    $"Singleton works, both variables contain the {singleton1.GetHashCode()} same {singleton2.GetHashCode()} instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
